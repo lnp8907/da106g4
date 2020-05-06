66430bdacd15275f4ebb209effdae314f4623323
erichsu1994123 <erichsu1994123@gmail.com>
2020-05-07 02:34:47 +0800

    嘉宏留言囉~ Android:Servlet新增QRcode及找商品的方法，修改Staff方法

--- src/android/com/staff/model/StaffDAO.java
+++ src/android/com/staff/model/StaffDAO.java
@@ -5,11 +5,11 @@ import java.util.List;
 import android.com.member.model.Member;
 
 public interface StaffDAO {
-	boolean isMember(String staff_account, String staff_password);
-	boolean isUserIdExist(String staff_account);
+	boolean isMember(String staff_id, String staff_password);
+	boolean isUserIdExist(String staff_id);
 //	boolean add(Member member);
 //	boolean update(Member member);
 //	boolean delete(String account);
-	Staff findById(String staff_account);
+	Staff findById(String staff_id);
 	List<Staff> getAll();
 }
