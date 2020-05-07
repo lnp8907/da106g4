427b8a39b139b73302919f8f82fb7d2aa34f1398
Nini <ninihour@g.ncu.edu.tw>
2020-05-07 14:34:12 +0800

    加1521

--- src/android/com/main/MyData.java
+++ src/android/com/main/MyData.java
@@ -1,7 +1,7 @@
 package android.com.main;
 
 public class MyData {
-	public static final String URL = "jdbc:oracle:thin:@localhost:XE";
+	public static final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
 	public static final String USER = "DA106_G4";
 	public static final String PASSWORD = "DA106_G4";
 }
