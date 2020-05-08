d42a71ca857c55b398fb0b9439f1d09ac961e2e5
Albert Ma <albert@ntou.edu.tw>
2020-05-08 13:51:44 +0800

    後台登入(要再修改) 靖祐

--- src/com/staff/model/BackEndLoginFilter.java
+++ src/com/staff/model/BackEndLoginFilter.java
@@ -26,28 +26,31 @@ public class BackEndLoginFilter implements Filter{
  @Override
  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
    throws IOException, ServletException {
+//	 System.out.println(1);
+//  HttpServletRequest req = (HttpServletRequest)request;
+//  HttpServletResponse res = (HttpServletResponse)response;
+//  HttpSession session = req.getSession();
+//  String staff_id = (String)session.getAttribute("staff_id");
+// if(staff_id == null) {
+//	
+////   session.setAttribute("location2", req.getRequestURI());
+//   System.out.println(2);
+//    // 登入換地址，此處也要換http://localhost:8081/DA106_G4_Foodporn_Git/backEnd2.jsp
+//   return;
+//  }else {
+//	  System.out.println(3);
+//   chain.doFilter(request, response);
 	 System.out.println(1);
   HttpServletRequest req = (HttpServletRequest)request;
   HttpServletResponse res = (HttpServletResponse)response;
   HttpSession session = req.getSession();
   String staff_id = (String)session.getAttribute("staff_id");
-  session.setAttribute("YES", 1);
-  String YES= (Integer)session.getAttribute("YES")+"";
-
   System.out.println(staff_id);
-  System.out.println("YES1"+YES);
-  if(YES != null) {
-	  res.sendRedirect(req.getContextPath() + "/backEnd_Login.jsp");
-	  session.setAttribute("YES", 0);
-	  return;
-  }
-  
-  
-  else if(staff_id == null) {
-	
-//   session.setAttribute("location2", req.getRequestURI());
+  if(staff_id == null) {
+	 
+   session.setAttribute("location2", req.getRequestURI());
    System.out.println(2);
-    // 登入換地址，此處也要換http://localhost:8081/DA106_G4_Foodporn_Git/backEnd2.jsp
+   res.sendRedirect(req.getContextPath() + "/backEnd_Login.jsp");  // 登入換地址，此處也要換
    return;
   }else {
 	  System.out.println(3);
